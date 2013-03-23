a748b7ccd778aaedc94fc0e31a81c38fdda17162
Kevin Darlington <kevin@outroot.com>
2013-03-23 14:48:43 -0400

    Strip debug from build. Update version and about.

--- ui/about.lua
+++ ui/about.lua
@@ -8,7 +8,6 @@ module("about", package.seeall)
 function show(version) 
   iup.Message("About MorphGEN", [[Made by Kevin and Cassandra Darlington
 Binary: http://github.com/kdar/morphgen-binary
-Code: http://github.com/kdar/morphgen
 Site: http://outroot.com
 
 Version: ]] .. version)
--- version.go
+++ version.go
@@ -11,7 +11,7 @@ var (
   VERSION = &version.Version{
     Major: 1,
     Minor: 0,
-    Patch: 1,
+    Patch: 2,
   }
 )
 
